e568ee4ff10e2967438a05dfc83e680672d5d1cc
Stas T <atqcpospravah@gmail.com>
2018-07-18 10:23:03 +0300

    inline styles example

--- 01_react_basics/src/components/header.js
+++ 01_react_basics/src/components/header.js
@@ -5,10 +5,22 @@ import React,{ Component } from 'react';
 
 
 class Header extends Component {
+    
     render() {
+        const styles = {
+            header:{
+                background: "#03a9f4"
+            },
+            logo:{
+                color: '#fff',
+                fontFamily: 'Anton',
+                textAlign: 'center'
+            }
+        }
+
         return (
-            <header>
-                <div>Logo</div>
+            <header style={styles.header} >
+                <div style={styles.logo}>Logo</div>
                 <input type='text'/>
             </header>
         )
