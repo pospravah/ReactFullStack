11af4dd88619632502e3b0d7f510b91613a39a24
Stas T <atqcpospravah@gmail.com>
2018-07-18 10:08:48 +0300

    Start of basic course added.

---
+++ 01_react_basics/src/components/header.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+// var header = {
+// }
+
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div>Logo</div>
+                <input type='text'/>
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ 01_react_basics/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+//COMPONENTS
+
+import Header from './components/header'
+
+const App = () =>{
+    return (
+        <div>
+            <Header/>
+        </div>
+    ) 
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root')); 
